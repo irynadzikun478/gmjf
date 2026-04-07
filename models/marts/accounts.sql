596042d6e2a488927304c2343b5d79ea5f012fa5
Iryna Dzikun <iryna.dzikun@belitsoft.com>
2026-04-07 12:42:35 +0000

    new column is aded

--- models/marts/accounts.sql
+++ models/marts/accounts.sql
@@ -10,7 +10,8 @@ transformed as (
 
     select
         account_id,
-        CONCAT(country,'_', city ) as geo_location
+        CONCAT(country,'_', city ) as geo_location,
+        DATEDIFF(day, created_at, CURRENT_DATE) AS days_since_created_date
     from source_accounts
 
 )
